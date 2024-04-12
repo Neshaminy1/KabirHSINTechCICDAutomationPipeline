e8d4c3e6f5dc39ec9ef8440756ad71005c1b6e41
Neshaminy1 <hkabir110@yahoo.com>
2024-04-11 21:05:00 -0400

    Run

---
+++ Tests/Practice/SeleniumTwo.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+
+namespace HSINTechCICDAutomationPipeline.Tests.Practice
+{
+    [TestClass]
+    public class SeleniumTwo
+    {
+        //Declare variable
+        public IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod_SeleniumTwo()
+        {
+            //*****************************************************************************
+
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Console.WriteLine("Open google home page");
+
+            driver.Manage().Window.Maximize();
+            Console.WriteLine("Maximize the window");
+
+            var searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Selenium ChromeDriver");
+            Console.WriteLine("Enter Selenium ChromeDriver into Search Text box");
+
+            searchBox.Submit();
+            Console.WriteLine("Click Search / Submit");
+            Thread.Sleep(3000);
+
+        }
+
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            driver.Quit();
+            Console.WriteLine("Close Browser");
+            Console.WriteLine("Quit Driver");
+        }
+
+
+    }
+}
+
