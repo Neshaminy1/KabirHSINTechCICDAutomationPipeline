c4c4af3741826cb243ef4489c570efb4e82ebd61
Neshaminy1 <hkabir110@yahoo.com>
2024-04-11 22:10:55 -0400

    Run Code in headless mode

--- Tests/Practice/SeleniumOne.cs
+++ Tests/Practice/SeleniumOne.cs
@@ -23,8 +23,15 @@ namespace HSINTechCICDAutomationPipeline.Tests.Practice
         {
             //*****************************************************************************
 
+
+            //========================== Headless Mode ==========================
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless=new");
+            driver = new ChromeDriver(options);
+            //====================================================
+
             new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
-            driver = new ChromeDriver();
+            //driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://www.google.com");
             Console.WriteLine("Open google home page");
